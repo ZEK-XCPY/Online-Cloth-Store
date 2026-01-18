696da1b6891429bdd00c80b2b8f8ab8b5949f646
ZEK-XCPY <zaid03297@gmail.com>
2026-01-18 12:15:09 +0530

    Connect Db

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+
+//App config
+const app = express();
+
+const port = process.env.PORT || 4000
+
+//middleware
+app.use(express.json())
+app.use(cors())
+connectDB();
+
+//api endpoints
+app.get('/',(req,res)=>{
+  res.send("API Working")
+})
+
+app.listen(port,()=>{
+  console.log('server started on port :' + port)
+})
