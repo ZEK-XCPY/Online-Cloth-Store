b1562fc92833e8d80229d4fcc91b12f8fdd6ce3c
ZEK-XCPY <zaid03297@gmail.com>
2026-01-18 20:34:17 +0530

    add controller and route for user and products

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+
+
+// function for adddProduct
+const addProduct = async (req,res)=>{
+
+}
+
+// func for list products
+const listProducts = async (req,res)=>{
+  
+}
+
+//func for removing prod
+const removeProduct = async (req,res)=>{
+  
+}
+
+// func for single product
+const singleProduct = async (req,res)=>{
+  
+}
+
+export {addProduct,listProducts,removeProduct,singleProduct}
